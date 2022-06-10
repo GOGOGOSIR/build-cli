cf97238678fd47064dca2f6941b7f74873424285
wanjianqiang <1239531712@qq.com>
2022-06-10 18:07:16 +0800

    docs: 新增文档

--- config/build-cli.js
+++ config/build-cli.js
@@ -1,6 +1,7 @@
 export default {
   projectName: '', // 要构建的项目名称
-  dependenciesWorkspace: 'BUILD_CLI_WORKSPACE', // 依赖项目的工作区，即：存放目录，BUILD_CLI_WORKSPACE 为node的环境变量，如果其不存在，则默认在 homedir
+  dependenciesWorkspace: 'BUILD_CLI_WORKSPACE',
+  distName: '',
   dependencies: [], // 其他依赖的项目配置
   buildCommand: {
     test: 'npm run build:test',
